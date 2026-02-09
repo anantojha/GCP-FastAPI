ba2d77dd43bd6586250f8dc4e1067998efc5c63e
anantojha <anantojha10@gmail.com>
2026-02-08 23:57:35 -0500

    health check

--- app/main.py
+++ app/main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
 
+@app.get("/")
 def hello(name: str = "World"):
    """Return a friendly HTTP greeting."""
    return {
       "message": f"Hello {name}!"
+   }
+
+
+@app.get("/health")
+def health():
+   """Return a HTTP health check."""
+   return {
+      "status": 200
    }
